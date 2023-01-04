920e975e72664e328abdcd405bd0a0682b08f2c7
Oğuz Kürşat Özkan <oguz.3027@gmail.com>
2023-01-04 13:45:53 +0300

    Child Interface updated

---
+++ src/device/AndroidApps.java
@@ -0,0 +1,8 @@
+package device;
+
+import boşpaket.Downloadable;
+
+public interface AndroidApps extends Downloadable {
+    public String AppStoreName = "GoogleStore";
+    public String OS = "Android";
+}
