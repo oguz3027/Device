9c7b87fe145720b5608f7eb1bdac366db9111392
Kerime Unal <kerime.cakalli@gmail.com>
2023-01-04 13:11:32 +0300

    phone class created

--- src/device/Phone.java
+++ src/device/Phone.java
@@ -16,7 +16,7 @@ public abstract class Phone extends Device{
         }
         this.phoneNumber = phoneNumber;
     }
-    // constructor
+    // constructor---
     public Phone(String brand, String model, double price, String color, String size, boolean hasBattery, boolean hasPowerButton,long phoneNumber) {
         super(brand, model, price, color, size, hasBattery, hasPowerButton);
         this.phoneNumber=phoneNumber;
