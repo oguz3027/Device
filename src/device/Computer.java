af0f5da9a124041314f00986e2a79ca4b168d11e
Akile Altin <altinakile10@gmail.com>
2023-01-04 13:19:11 +0300

    computer class

---
+++ src/device/Computer.java
@@ -0,0 +1,9 @@
+package device;
+
+public abstract class Computer extends Device {
+
+    public Computer(String brand, String model, double price, String color, String size, boolean hasBattery, boolean hasPowerButton) {
+        super(brand, model, price, color, size, hasBattery, hasPowerButton);
+    }
+
+}
